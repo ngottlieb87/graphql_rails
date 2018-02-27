13f58848447a1dec68393a1f4f27ad705d704090
ngottlieb87 <nogpdx@gmail.com>
2018-02-27 15:51:32 -0800

    add seed files for projects and users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,15 @@
 PROJECT_TITLES = 10.times.map { Faker::Internet.domain_word}
-puts titles
+
+SPLIT = 0.4
+OFFSET = PROJECT_TITLES.length * SPLIT
+
+PROJECT_USERS = {
+  'small@taco.com' => PROJECT_TITLES[0, OFFSET],
+  'big@burrito.com' => PROJECT_TITLES[OFFSET..-1]
+}
+
+PROJECT_USERS.each do |email, projects|
+  user = User.new(email: email, password: 'secret')
+  projects.each { |title| user.projects.build(title:title)}
+  user.save
+end
